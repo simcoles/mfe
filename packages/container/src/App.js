98392f3c19be0b3979e045bc7fe4d76f706892a1
Simeon Coles <simeoncoles@Simeons-MacBook-Pro.local>
2023-08-27 18:52:01 +0100

    Added Header component

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
-
+import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "../components/MarketingApp";
+import Header from "../components/Header";
 
 export default () => {
-  return <MarketingApp />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <MarketingApp />
+    </BrowserRouter>
+  );
 };
