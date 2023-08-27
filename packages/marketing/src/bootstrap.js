99abe154d4b2ff8db13a7efd631f95a18a4d67e2
Simeon Coles <simeoncoles@Simeons-MacBook-Pro.local>
2023-08-27 18:34:00 +0100

    comment back

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -5,7 +5,7 @@ import App from "./App";
 const mount = (el) => {
   ReactDOM.render(<App />, el);
 };
-
+// comment
 if (process.env.NODE_ENV === "development") {
   const el = document.querySelector("#_marketing-dev-root");
 
