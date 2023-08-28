655d6950232f8b70aedd62cd04788a391a7c2992
Simeon Coles <simeoncoles@Simeons-MacBook-Pro.local>
2023-08-28 09:58:34 +0100

    Resolved css naming conflicts in production

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  Stylesprovider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import MarketingApp from "../components/MarketingApp";
 import Header from "../components/Header";
 
 export default () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+  });
   return (
     <BrowserRouter>
-      <Header />
-      <MarketingApp />
+      <Stylesprovider createGenerateClassName={generateClassName}>
+        <Header />
+        <MarketingApp />
+      </Stylesprovider>
     </BrowserRouter>
   );
 };
